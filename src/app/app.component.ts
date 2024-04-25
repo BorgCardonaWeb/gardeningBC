f0362e8e34f692a900ab7dd236b96b25aa6f36db
BorgCardonaWeb <167750716+BorgCardonaWeb@users.noreply.github.com>
2024-04-25 15:59:01 +0200

    Banner info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterEvent, RouterLink, RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HeaderComponent } from './features/components/header/header.component';
+import { PrincipalBannerComponent } from './features/components/principal-banner/principal-banner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   //imports: [RouterOutlet, FontAwesomeModule, RouterLink],
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, PrincipalBannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/features/components/principal-banner/principal-banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-principal-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './principal-banner.component.html',
+  styleUrl: './principal-banner.component.css'
+})
+export class PrincipalBannerComponent {
+
+}
