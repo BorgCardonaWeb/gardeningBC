c5e627dedf341159fde7e247580f31330ae1f0d9
BorgCardonaWeb <167750716+BorgCardonaWeb@users.noreply.github.com>
2024-04-26 15:56:33 +0200

    Search controll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,16 @@ import { RouterEvent, RouterLink, RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HeaderComponent } from './features/components/header/header.component';
 import { PrincipalBannerComponent } from './features/components/principal-banner/principal-banner.component';
+import { SearcherComponent } from './features/components/searcher/searcher.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   //imports: [RouterOutlet, FontAwesomeModule, RouterLink],
-  imports: [HeaderComponent, PrincipalBannerComponent],
+  imports: [HeaderComponent,
+    PrincipalBannerComponent,
+    SearcherComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
