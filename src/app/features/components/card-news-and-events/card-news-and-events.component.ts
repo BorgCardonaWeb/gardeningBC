b51984e2aae809e3c4c5973c2f1646bd6b11b81c
Xiomara Andrea <xiomaraa.pulido@gmail.com>
2025-04-11 12:01:14 +0200

    Ajuste estilos

---
+++ src/app/features/components/card-news-and-events/card-news-and-events.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-news-and-events',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-news-and-events.component.html',
+  styleUrl: './card-news-and-events.component.css'
+})
+export class CardNewsAndEventsComponent {
+
+}
